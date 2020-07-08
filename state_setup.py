9f4338a8e176b74542e2e304448831d3ab35fdca
Danny Anslinger <danslinger@users.noreply.github.com>
2020-07-08 12:40:40 -0700

    add states setup

---
+++ state_setup.py
@@ -0,0 +1,15 @@
+import os
+from app.models.states import States
+from app import db, create_app
+
+def main():
+    # noinspection PyUnusedLocal
+    create_app(os.getenv('FLASK_CONFIG')
+               or 'default').app_context().push()
+    states = [States(name="biddingOn"), States(name="franchiseDecisionMade"), States(name="transitionDecisionMade")]
+    db.session.add_all(states)
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    main()
