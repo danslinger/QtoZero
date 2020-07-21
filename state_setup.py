9c25d3a772bac439fac2a967ffad7410c26974d8
Danny Anslinger <danslinger@users.noreply.github.com>
2020-07-20 22:17:37 -0700

    Fix state setup bug

--- state_setup.py
+++ state_setup.py
@@ -2,11 +2,13 @@ import os
 from app.models.states import States
 from app import db, create_app
 
+
 def main():
     # noinspection PyUnusedLocal
     create_app(os.getenv('FLASK_CONFIG')
                or 'default').app_context().push()
     states = [States(name="biddingOn"), States(name="franchiseDecisionMade"), States(name="transitionDecisionMade")]
+    States.query.delete()
     db.session.add_all(states)
     db.session.commit()
 
