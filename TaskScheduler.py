918b08dfa5a1cbec51da13b488931d630f62947b
Danny Anslinger <danslinger@users.noreply.github.com>
2020-07-29 21:41:25 -0700

    Make TaskScheduler safe when testing in Windows

--- TaskScheduler.py
+++ TaskScheduler.py
@@ -3,11 +3,17 @@ from crontab import CronTab
 
 class TaskScheduler(object):
     """docstring for TaskScheduler"""
+
     def __init__(self):
         super(TaskScheduler, self).__init__()
-        self.cron = CronTab(user="danny")
+        try:
+            self.cron = CronTab(user="danny")
+        except:
+            self.cron = None
 
     def get_job(self, comment, command=''):
+        if self.cron is None:
+            return
         jobs = [job for job in self.cron.find_comment(comment)]
         if jobs:
             return jobs[0]
@@ -15,5 +21,7 @@ class TaskScheduler(object):
             return self.cron.new(command=command, comment=comment)
 
     def set_job(self, job, date):
+        if self.cron is None:
+            return
         job.setall(date)
         self.cron.write()
